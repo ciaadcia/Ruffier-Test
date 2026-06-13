dc864ebe7b34c46bcd94aab55a397ab153e8cdeb
zirakerenbgt <hazirah.humaira.azzahra@gmail.com>
2026-06-13 16:38:46 +0700

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -52,3 +52,52 @@ def initUI(self): #tambahkan
   self.h_line.addLayout(self.l_line)
   self.h_line.addLayout(self.r_line)
   self.setLayout(self.h_line)
+
+#pertemuan kedua
+def timer_test(self):
+  global time
+  time = QTime (0, 0, 15)
+  self.timer = QTimer()
+  self.timer.timeout.connect(self.timer1Event)
+  self.timer.start(1000)
+def timer_sits(self):
+  global time
+  time = QTime (0, 0, 30)
+  self.timer = QTimer()
+  self.timer.timeout.connect(self.timer2Event)
+  self.timer.start(1500)
+def timer_final(self):
+  global time
+  time = QTime (0, 1, 0)
+  self.timer = QTimer()
+  self.timer.timeout.connect(self.timer3Event)
+  self.timer.start(1000)
+def timer1Event(self):
+  global time
+  time = time.addSecs(-1)
+  self.text_timer.setText(time.toString("hh:mm:ss"))
+  self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+  self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+  if time.toString("hh:mm:ss") == "00:00:00":
+    self.timer.stop()
+def timer2Event(self):
+  global time
+  time = time.addSecs(-1)
+  self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
+  self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+  self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+  if time.toString("hh:mm:ss") == "00:00:00":
+    self.timer.stop()
+def timer3Event(self):
+  global time
+  time = time.addSecs(-1)
+  self.text_timer.setText(time.toString("hh:mm:ss"))
+  if int(time.toString("hh:mm:ss")[6:8]) >= 45:
+    self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+  elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
+    self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+  else:
+    self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+  self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+  if time.toString("hh:mm:ss") == "00:00:00":
+    self.timer.stop()
